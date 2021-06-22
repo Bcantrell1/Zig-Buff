3447543910623c65f8fd57bc5d917c16a021249b
Brian Cantrell <bribri546@gmail.com>
2021-06-21 23:49:06 -0700

    Log out and refresh handlers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import jwt from "jwt-decode";
 
 export default function App() {
     const [currentUser, setCurrentUser] = useState(null);
@@ -13,14 +14,37 @@ export default function App() {
         if (window.focus) popupWindow.focus();
     };
 
+    const handleLogout = () => {
+        console.log("Logging Out");
+        localStorage.clear();
+        setCurrentUser(null);
+    };
+
+    const checkExpiredToken = () => {
+        let token = localStorage.getItem("jwtToken");
+        if (token) {
+            let decoded = jwt(token);
+            if (decoded.exp * 1000 < Date.now()) {
+                console.log("Token Expired");
+                localStorage.clear();
+                setCurrentUser(null);
+            } else {
+                if (!currentUser) {
+                    setCurrentUser(decoded.user);
+                }
+            }
+        }
+    };
+
     useEffect(() => {
+        checkExpiredToken();
+
         window.addEventListener("message", (event) => {
             if (event.origin !== "http://localhost:5460") return;
             const { token, ok } = event.data;
             const user = event.data.user;
             if (ok) {
                 localStorage.setItem("jwtToken", token);
-                console.log(JSON.parse(user));
                 setCurrentUser(JSON.parse(user));
             }
         });
@@ -29,6 +53,9 @@ export default function App() {
     return !currentUser ? (
         <button onClick={handleLogin}>Login</button>
     ) : (
-        <div>{currentUser.id}</div>
+        <>
+            <div>{currentUser.id}</div>
+            <button onClick={handleLogout}>Log Out</button>
+        </>
     );
 }
